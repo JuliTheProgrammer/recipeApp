9d99ac1602e564519d6ed1fbd6141b7e87acfceb
Julian <juli.leipert@gmail.com>
2023-10-11 10:45:10 +0200

    Initial Commit

---
+++ recipe-list-replica/recipe_list_replicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_list_replicaApp.swift
+//  recipe-list-replica
+//
+//  Created by Julian Leipert on 11.10.23.
+//
+
+import SwiftUI
+
+@main
+struct recipe_list_replicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
